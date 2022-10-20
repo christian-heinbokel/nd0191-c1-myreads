eed5c51bab3113085877969882a02520de8ab4a5
Lennart Heinbokel <lennart@heinbokel.net>
2022-10-20 20:38:41 +0200

    refactors out bookshelf component

--- starter/src/components/BookShelf.js
+++ starter/src/components/BookShelf.js
@@ -1,7 +1,25 @@
-const BookShelf = () => {
+import Book from "./Book";
+
+const BookShelf = ({ bookshelfTitle, books }) => {
   return (
-    <div>
-      <h1>Book Shelf</h1>
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{bookshelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => {
+            return (
+              <li>
+                <Book
+                  key={book.title}
+                  title={book.title}
+                  authors={book.authors}
+                  backgroundImageUrl={book.backgroundImageUrl}
+                />
+              </li>
+            );
+          })}
+        </ol>
+      </div>
     </div>
   );
 };
