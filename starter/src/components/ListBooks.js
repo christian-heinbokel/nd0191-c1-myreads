e8c4c0af33001c62a80de9bcf827cda9dc5acddf
Lennart Heinbokel <lennart@heinbokel.net>
2022-10-20 23:14:33 +0200

    books are now received from api rather than hardcoding them

--- starter/src/components/ListBooks.js
+++ starter/src/components/ListBooks.js
@@ -2,11 +2,24 @@ import ListBooksContent from "./ListBooksContent";
 import ListBooksTitle from "./ListBooksTitle";
 import OpenSearchButton from "./OpenSearchButton";
 
+import { useEffect, useState } from "react";
+import * as BooksAPI from "../BooksAPI";
+
 const ListBooks = ({ showSearchPage, setShowSearchpage }) => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    async function fetchBooks() {
+      let books = await BooksAPI.getAll();
+      setBooks(books);
+      console.log("received books: ", books);
+    }
+    fetchBooks();
+  }, []);
   return (
     <div className="list-books">
       <ListBooksTitle />
-      <ListBooksContent />
+      <ListBooksContent books={books} />
       <OpenSearchButton
         setShowSearchpage={setShowSearchpage}
         showSearchpage={showSearchPage}
