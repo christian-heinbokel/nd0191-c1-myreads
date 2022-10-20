d91da72a78b7660416037f487e33d42f2b8f103b
Lennart Heinbokel <lennart@heinbokel.net>
2022-10-20 20:30:33 +0200

    refactors out Book component

---
+++ starter/src/components/Book.js
@@ -0,0 +1,31 @@
+const Book = ({ backgroundImageUrl, title, authors }) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 192,
+            backgroundImage: `url("${backgroundImageUrl}")`,
+          }}
+        ></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors}</div>
+    </div>
+  );
+};
+
+export default Book;
